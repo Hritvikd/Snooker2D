2f79008ccd741cac4df3c5322fbed2c6d8bc6b9d
Hritvik Dekate <hritvikd95@gmail.com>
2021-10-26 20:34:59 +0530

    SnookerGame

---
+++ Assets/Script/Ballscript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballscript : MonoBehaviour
+{
+    public float power = 10f;
+    public float maxDrag = 5f;
+    public Rigidbody2D rb;
+    //public Text GameOver;
+    //public Text restart;
+    //public Text score;
+    //public GameObject gb;
+    //public GameObject button;
+
+    //public GameObject nextBall;
+    //public static bool destroy = false;
+    Vector3 dragStartPos;
+    Vector2 initpos;
+    Touch touch;
+    private void Start()
+    {
+        initpos = rb.position;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                DragStart();
+            }
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Dragging();
+            }
+            if (touch.phase == TouchPhase.Ended)
+            {
+                DragRelease();
+            }
+        }
+    }
+    void DragStart()
+    {
+        dragStartPos = Camera.main.ScreenToWorldPoint(touch.position);
+        dragStartPos.z = 0f;
+    }
+    void Dragging()
+    {
+        Vector3 draggingPos = Camera.main.ScreenToWorldPoint(touch.position);
+        draggingPos.z = 0f;
+    }
+    void DragRelease()
+    {
+        Vector3 dragReleasePos = Camera.main.ScreenToWorldPoint(touch.position);
+        dragReleasePos.z = 0f;
+
+        Vector3 force = dragStartPos - dragReleasePos;
+        Vector3 clampedForce = Vector3.ClampMagnitude(force, maxDrag) * power;
+        rb.AddForce(clampedForce, ForceMode2D.Impulse);
+
+
+    }
+}
---
+++ Assets/Script/Holescript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Holescript : MonoBehaviour
+{
+    public int score = 0;
+    public Text ScoreText;
+    int life = 3;
+    public Rigidbody2D gb;
+    Vector2 initpos;
+
+    private Collider2D col;
+    private void Start()
+    {
+        initpos = gb.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Normalball"))
+        {
+            AddScore();
+            col = collision;
+            
+            Destroy(collision.gameObject);
+            //Invoke("waitmethod", 5f);
+
+        }
+        if (collision.gameObject.CompareTag("Cueball"))
+        {
+            //Destroy(collision.gameObject);
+            //Resetball();
+            gb.position = initpos;
+            gb.velocity = new Vector2(0, 0);
+            life = life - 1;
+        }
+            
+        
+    }
+    private void AddScore()
+    {
+       
+            score = score + 5;
+            ScoreText.text = score.ToString();
+      
+    }
+
+    void Update()
+    {
+        if (life == 0)
+        {
+            //End Game
+            SceneManager.LoadScene(3);
+        }
+
+        if (score >= 15)
+        {
+            //Won
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        }
+
+    }
+    
+}
